9b115e8b0bd71250b5be12c824dc5d395f4699ee
Arofat Nizamova <arofatnizamova@gmail.com>
2026-07-01 15:09:21 +0500

    feat: Industries & Standards add

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $('.slick-slider').each(function () {
+$(document).ready(function() {
+    $('.slick-slider').each(function() {
         let slider = $(this);
 
         let options = {
@@ -12,121 +12,32 @@ $(document).ready(function () {
 
         let extraOptions = {};
 
-        if (slider.hasClass('products')) {
+        if (slider.hasClass('standarts')) {
             extraOptions = {
-                slidesToShow: 4,
+                slidesToShow: 6.3,
                 slidesToScroll: 1,
                 centerMode: false,
                 arrows: true,
                 dots: false,
                 autoplay: true,
-                responsive: [
-                    {
+                responsive: [{
                         breakpoint: 1200,
                         settings: {
-                            slidesToShow: 3,
+                            slidesToShow: 4.5,
                             slidesToScroll: 1,
                         }
                     },
                     {
                         breakpoint: 992,
                         settings: {
-                            slidesToShow: 2,
+                            slidesToShow: 4.3,
                             slidesToScroll: 1,
                         }
                     },
                     {
                         breakpoint: 576,
                         settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        }
-                    },
-                ]
-            };
-        }
-
-        if (slider.hasClass('sertificates')) {
-            extraOptions = {
-                slidesToShow: 4.2,
-                slidesToScroll: 1,
-                centerMode: false,
-                arrows: true,
-                dots: false,
-                autoplay: true,
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 3.5,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2.8,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        }
-                    },
-                ]
-            };
-        }
-
-        if (slider.hasClass('categories')) {
-            extraOptions = {
-                slidesToShow: 6,
-                slidesToScroll: 1,
-                infinite: false,
-                centerMode: false,
-                arrows: true,
-                dots: false,
-                autoplay: false,
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 5,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 4.1,
-                            slidesToScroll: 1,
-                        }
-                    },
-                ]
-            };
-        } if (slider.hasClass('categories-2')) {
-            extraOptions = {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                infinite: false,
-                centerMode: false,
-                arrows: true,
-                dots: false,
-                autoplay: false,
-                responsive: [
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 4.1,
+                            slidesToShow: 1.7,
                             slidesToScroll: 1,
                         }
                     },
